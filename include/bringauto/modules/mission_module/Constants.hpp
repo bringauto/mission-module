569006eca27b39e2c3c543c224f26d9b97c8e9e6
MarioIvancik <mario.ivancik@bringauto.com>
2024-07-03 09:50:23 +0200

    remove inline from constants

--- include/bringauto/modules/mission_module/Constants.hpp
+++ include/bringauto/modules/mission_module/Constants.hpp
@@ -13,12 +13,12 @@ public:
     /**
      * @brief The speed threshold for the status to be sent periodically
      */
-    inline static constexpr double status_speed_threshold = 1.0;
+    static constexpr double status_speed_threshold = 1.0;
 
     /**
      * @brief Minimum time between two status messages being sent when speed is above the threshold
      */
-    inline static constexpr std::chrono::milliseconds status_sending_period = std::chrono::milliseconds(2900);
+    static constexpr std::chrono::milliseconds status_sending_period = std::chrono::milliseconds(2900);
 };
 
 }
