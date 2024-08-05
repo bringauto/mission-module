9dae30a8761d565f7d897c4f19ccad7f6af30563
MarioIvancik <mario.ivancik@bringauto.com>
2024-08-05 12:00:15 +0200

    use protobuf only for message validation

---
+++ include/bringauto/ba_json/JsonHelper.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <fleet_protocol/common_headers/memory_management.h>
+
+#include <MissionModule.pb.h>
+
+#include <nlohmann/json.hpp>
+
+namespace bringauto::ba_json {
+
+using json = nlohmann::ordered_json;
+
+class JsonHelper {
+public:
+	static int bufferToJson(json* json, const buffer& buffer);
+
+	static int jsonToBuffer(buffer* buffer, const json& json);
+
+	static MissionModule::AutonomyStatus_State stringToAutonomyState(const std::string &state);
+
+	static std::string autonomyStateToString(MissionModule::AutonomyStatus_State state);
+
+};
+}
