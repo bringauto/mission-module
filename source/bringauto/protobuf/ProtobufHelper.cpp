5f4f9aa327e3ab65d4913835ea464d6052b152dc
MarioIvancik <mario.ivancik@bringauto.com>
2024-08-12 10:51:47 +0200

    debug logging

--- source/bringauto/protobuf/ProtobufHelper.cpp
+++ source/bringauto/protobuf/ProtobufHelper.cpp
@@ -3,8 +3,17 @@
 #include <bringauto/protobuf/ProtobufHelper.hpp>
 
 #include <google/protobuf/util/json_util.h>
+#include <bringauto/logging/ConsoleSink.hpp>
+#include <bringauto/logging/Logger.hpp>
 
 namespace bringauto::protobuf {
+
+using namespace bringauto::logging;
+static constexpr LoggerId context = {.id = "id"};
+using Logger1 = Logger<context, LoggerImpl>;
+//Logger1::addSink<ConsoleSink>();
+//Logger1::init(LoggerSettings {"MissionModule", LoggerVerbosity::Debug});
+
 int ProtobufHelper::serializeProtobufMessageToBuffer(struct buffer* message, const google::protobuf::Message &protobufMessage) {
 	if (allocate(message, protobufMessage.ByteSizeLong()) == OK) {
 		protobufMessage.SerializeToArray(message->data, static_cast<int>(message->size_in_bytes));
@@ -14,10 +23,14 @@ int ProtobufHelper::serializeProtobufMessageToBuffer(struct buffer* message, con
 }
 
 int ProtobufHelper::validateAutonomyStatus(const std::string &status) {
+	Logger1::addSink<ConsoleSink>();
+	Logger1::init(LoggerSettings {"MissionModule", LoggerVerbosity::Debug});
+	Logger1::logDebug("status: {}", status);
 	MissionModule::AutonomyStatus autonomyStatus {};
 	const auto parse_status = google::protobuf::util::JsonStringToMessage(
 		status, &autonomyStatus
 	);
+	Logger1::destroy();
 	if(!parse_status.ok()) {
 		return NOT_OK;
 	}
@@ -25,10 +38,14 @@ int ProtobufHelper::validateAutonomyStatus(const std::string &status) {
 }
 
 int ProtobufHelper::validateAutonomyCommand(const std::string &command) {
+	Logger1::addSink<ConsoleSink>();
+	Logger1::init(LoggerSettings {"MissionModule", LoggerVerbosity::Debug});
+	Logger1::logDebug("command: {}", command);
 	MissionModule::AutonomyCommand autonomyCommand {};
 	const auto parse_status = google::protobuf::util::JsonStringToMessage(
 		command, &autonomyCommand
 	);
+	Logger1::destroy();
 	if(!parse_status.ok()) {
 		return NOT_OK;
 	}
