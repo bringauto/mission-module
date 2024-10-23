cd6f743a61a0a4183731f2bc1e47d6c337b87bdd
MarioIvancik <mario.ivancik@bringauto.com>
2024-10-23 11:32:47 +0200

    fix config value checking

--- source/external_server_api.cpp
+++ source/external_server_api.cpp
@@ -58,28 +58,28 @@ void *init(const config config_data) {
         }
         else if (i->first == "max_requests_threshold_count") {
             auto result = std::from_chars(i->second.data(), i->second.data() + i->second.size(), max_requests_threshold_count);
-            if (result.ec == std::errc() || max_requests_threshold_count < 0 || i->second.empty()) {
+            if (result.ec != std::errc() || max_requests_threshold_count < 0 || i->second.empty()) {
                 delete context;
                 return nullptr;
             }
         }
         else if (i->first == "max_requests_threshold_period_ms") {
             auto result = std::from_chars(i->second.data(), i->second.data() + i->second.size(), max_requests_threshold_period_ms);
-            if (result.ec == std::errc() || max_requests_threshold_period_ms < 0 || i->second.empty()) {
+            if (result.ec != std::errc() || max_requests_threshold_period_ms < 0 || i->second.empty()) {
                 delete context;
                 return nullptr;
             }
         }
         else if (i->first == "delay_after_threshold_reached_ms") {
             auto result = std::from_chars(i->second.data(), i->second.data() + i->second.size(), delay_after_threshold_reached_ms);
-            if (result.ec == std::errc() || delay_after_threshold_reached_ms < 0 || i->second.empty()) {
+            if (result.ec != std::errc() || delay_after_threshold_reached_ms < 0 || i->second.empty()) {
                 delete context;
                 return nullptr;
             }
         }
         else if (i->first == "retry_requests_delay_ms") {
             auto result = std::from_chars(i->second.data(), i->second.data() + i->second.size(), retry_requests_delay_ms);
-            if (result.ec == std::errc() || retry_requests_delay_ms < 0 || i->second.empty()) {
+            if (result.ec != std::errc() || retry_requests_delay_ms < 0 || i->second.empty()) {
                 delete context;
                 return nullptr;
             }
