d2259301931acef16f37b0b1d197006faf83f03e
jiri-skuta <jiri.skuta@bringauto.com>
2026-03-25 00:45:05 +0100

    Updated dependencies to match rebuilt fleet-http-client-shared lib in latest package-repo

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,13 +1,15 @@
 SET(CMAKE_FIND_USE_CMAKE_SYSTEM_PATH FALSE)
 
 BA_PACKAGE_LIBRARY(fleet-protocol-interface          v2.0.0 NO_DEBUG ON)
-BA_PACKAGE_LIBRARY(fleet-protocol-cxx-helpers-static v1.1.1)
+BA_PACKAGE_LIBRARY(fleet-protocol-cpp                v1.2.0)
+BA_PACKAGE_LIBRARY(async-function-execution          v1.0.0)
+BA_PACKAGE_LIBRARY(aeron                             v1.48.6)
 BA_PACKAGE_LIBRARY(zlib                              v1.2.11)
 BA_PACKAGE_LIBRARY(nlohmann-json                     v3.10.5 NO_DEBUG ON)
 BA_PACKAGE_LIBRARY(protobuf                          v4.21.12)
 
 IF (FLEET_PROTOCOL_BUILD_EXTERNAL_SERVER)
-    BA_PACKAGE_LIBRARY(fleet-http-client-shared v2.0.0)
+    BA_PACKAGE_LIBRARY(fleet-http-client-shared v2.0.1)
     BA_PACKAGE_LIBRARY(boost                    v1.86.0)
     BA_PACKAGE_LIBRARY(cpprestsdk               v2.10.20)
 ENDIF ()
